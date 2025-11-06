eab45e506ba4dd2839ce980eefc63fe5e672b9a6
Leonardo Kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2025-11-06 11:57:15 +0000

    js

---
+++ script.js
@@ -0,0 +1,43 @@
+// Seleciona os elementos das luzes 
+const luzVermelha = document.getElementById("vermelho");
+const luzAmarela = document.getElementById("amarelo");
+const luzVerde = document.getElementById("verde");
+
+// Botoẽs
+
+const btnVermelho = document.getElementById("btnVermelho");
+const btnAmarelo = document.getElementById("btnAmarelo");
+const btnVerde = document.getElementById("btnVerde");
+const btnAutomático = document.getElementById("btnAutomático");
+const btnParar = document.getElementById("btnParar");
+
+// Variável para o modo automático
+let intervalo;
+
+function limpar(){
+    luzVermelha.className = 'luz';
+    luzAmarela.className = 'luz';
+    luzVerde.className = 'luz';
+}
+
+function acenderVermelho(){
+    limpar();
+    luzVermelha.classList.add("acesa","vermelha");
+}
+
+function acenderAmarelo(){
+    limpar();
+    luzAmarela.classList.add("acesa", "amarela")
+}
+
+function acenderVerde(){
+    limpar();
+    luzVerde.classList.add("acesa","verde")
+}
+
+function modoAutomatico(){
+    limpar();
+    let estado = 0;
+    intervalo = set
+}
+
